f19cc265aea29111acab9a92df7a96b43c965556
Mel Booth <mel.m.booth@gmail.com>
2016-11-06 15:27:46 +1300

    set default canvas style

--- draw.js
+++ draw.js
@@ -1,2 +1,8 @@
 var canvas = document.getElementbyId('paper')
 var context = canvas.getContext('2d')
+
+function defaultCanvas(){
+  context.lineWidth = 5
+  context.lineCap = 'round'
+  context.strokeStyle = '#000000'
+}
