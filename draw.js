4efc54b61045969a76ae5b9cd2828e1fbfdb32b5
Mel Booth <mel.m.booth@gmail.com>
2016-11-06 15:33:34 +1300

    set up event handlers for mouse

--- draw.js
+++ draw.js
@@ -6,3 +6,26 @@ function defaultCanvas(){
   context.lineCap = 'round'
   context.strokeStyle = '#000000'
 }
+
+function onMouseUp(evt){
+  mouseup = false
+}
+
+function onMouseDown(evt){
+  mousedown = true
+
+}
+
+function onMouseMove(evt){
+  var x = evt.clientX
+  var y = evt.clientY
+  if (mousedown){
+    draw(x, y)
+  }
+}
+
+function draw(x, y){
+
+}
+
+defaultCanvas()
