6c051e8119eb99b2a6f297bf3c9f8510a1078c3b
Mel Booth <mel.m.booth@gmail.com>
2016-11-09 13:55:16 +1300

    update index.html and draw.html to simplify

--- paper.js
+++ paper.js
@@ -8,7 +8,7 @@ canvas.addEventListener('mousemove', function(evt) {
   mouse.y = evt.pageY - this.offsetTop;
 }, false);
 
-context.lineWidth = 3;
+context.lineWidth = 2;
 context.lineCap = 'round';
 context.strokeStyle = '#000000';
 
@@ -26,4 +26,16 @@ canvas.addEventListener('mouseup', function() {
 var draw = function() {
     context.lineTo(mouse.x, mouse.y);
     context.stroke();
+}
+
+// var drawButton = document.getElementById('draw').addEventListener('click', previousImageData){
+//   var previousImageData = context.getImageData(left, top, 500px, 10px)
+// })
+
+//How to show certain area of canvas...doesn't work. Yet
+var drawButton = document.getElementById("draw")
+draw.addEventListener("click", previousImageData())
+
+var previousImageData = function(context){
+  context.getImageData(0px, 0px, 500px, 20px)
 }
