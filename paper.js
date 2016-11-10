5d6f59699b8c4ecc2b2b7dbc1851867914df6c98
Mel Booth <mel.m.booth@gmail.com>
2016-11-11 10:42:32 +1300

    save a drawing to index - wip

--- paper.js
+++ paper.js
@@ -3,6 +3,13 @@ var context = canvas.getContext('2d');
 var images = []
 
 var mouse = {x: 0, y: 0};
+var test = document.createElement('img')
+test.src = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAfQAAAGQCAYAAABYs5LGAAAcvUlEQ…QIECBQIyDQa9yNSoAAAQIEUgUEeiqnYgQIECBAoEbg/wFWiy6vijmVoAAAAABJRU5ErkJggg==')"
+console.log('adding a test image');
+
+// 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAfQAAAGQCAYAAABYs5LGAAAcvUlEQ…QIECBQIyDQa9yNSoAAAQIEUgUEeiqnYgQIECBAoEbg/wFWiy6vijmVoAAAAABJRU5ErkJggg=='
+
+document.body.appendChild(test)
 
 canvas.addEventListener('mousemove', function(evt) {
   mouse.x = evt.pageX - this.offsetLeft;
@@ -32,6 +39,7 @@ var draw = function() {
 document.getElementById('Save').onclick=function(e){
   e.preventDefault()
   var img = canvas.toDataURL("image/png")
+  console.log(img);
   document.write('<img src="'+img+'"/>')
 }
 
@@ -41,3 +49,9 @@ document.getElementById('Save').onclick=function(e){
 //   var img = canvas.toDataURL("image/png")
 //   images.push(img)
 // }
+
+function convertCanvasToImage(canvas) {
+var image = new Image();
+image.src = canvas.toDataURL("image/png");
+return image;
+}
